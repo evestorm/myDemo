96a84ea26c9051e100f49d44c0b34b4b47451307
杨亮 <1572534835@qq.com>
2017-02-05 20:13:24 +0800

    系统自带分享(微博)

--- Share(分享)/Share/Share/ViewController.m
+++ Share(分享)/Share/Share/ViewController.m
@@ -7,22 +7,47 @@
 //
 
 #import "ViewController.h"
+#import <Social/Social.h>
 
 @interface ViewController ()
 
 @end
 
-@implementation ViewController
+@implementation ViewController {
+    UIButton *_systemBtn;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    _systemBtn = [UIButton buttonWithType:(UIButtonTypeContactAdd)];
+    _systemBtn.frame = CGRectMake(20, 20, 100, 100);
+    [_systemBtn addTarget:self action:@selector(modalSystemShare) forControlEvents:(UIControlEventTouchUpInside)];
+    [self.view addSubview:_systemBtn];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)modalSystemShare {
+    if (![SLComposeViewController isAvailableForServiceType:SLServiceTypeSinaWeibo]) {
+        NSLog(@"平台不可用");
+        return;
+    }
+    
+    SLComposeViewController *vc = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeSinaWeibo];
+    
+    [vc setInitialText:@"代颜街"];
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"logo_daiyanjie@2x.png" ofType:nil];
+    [vc addImage:[UIImage imageWithContentsOfFile:path]];
+    
+    [self presentViewController:vc animated:YES completion:nil];
+    
+    // 监听用户点击
+    vc.completionHandler = ^(SLComposeViewControllerResult result) {
+        if (result == SLComposeViewControllerResultCancelled) {
+            NSLog(@"用户点击了取消");
+        } else {
+            NSLog(@"点击了发布");
+        }
+    };
 }
 
 
