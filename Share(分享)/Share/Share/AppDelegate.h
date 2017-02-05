4669e3ea2c90b6495f0fb91617b8cabd8edb019f
杨亮 <1572534835@qq.com>
2017-02-05 19:46:42 +0800

    分享init

---
+++ Share(分享)/Share/Share/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Share
+//
+//  Created by evestorm on 2017/2/5.
+//  Copyright © 2017年 eve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
